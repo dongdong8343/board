c78c326f460c4ed624680117a0a4449214612c98
dongdong8343 <fkdlem8343@naver.com>
2025-04-17 13:11:17 +0900

    Initial commit

---
+++ src/test/java/org/dongdong/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dongdong.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
