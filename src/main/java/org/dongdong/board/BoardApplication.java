009d296377a890e93f3b65a57dfc1f73e83b3a55
donggeun-yu <93115530+dongdong8343@users.noreply.github.com>
2025-04-18 09:39:22 +0900

    Revert "Feature/junseo"

--- src/main/java/org/dongdong/board/BoardApplication.java
+++ src/main/java/org/dongdong/board/BoardApplication.java
@@ -2,10 +2,8 @@ package org.dongdong.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class BoardApplication {
 
 	public static void main(String[] args) {
